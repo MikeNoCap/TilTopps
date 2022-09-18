3cc2d205080cdb8ecdb9190cd712c37c68cb6499
Mike <thorvaldsenmikkel@gmail.com>
2022-09-18 18:26:18 +0200

    added more operations with brackets

--- main.cpp
+++ main.cpp
@@ -625,7 +625,9 @@ void operations(vector<int> nums, string answers[])
 
       {(pow(n1, n2) + n3) * n4 - n5, "(n1^n2+n3)*n4-n5"},
       {(n1 * n2 + n3) * (n4 + n5), "(n1*n2+n3)*(n4+n5)"},
-      {(n1 * n2 + n3) * n4 + n5, "(n1*n2+n3)*n4+n5"}
+      {(n1+n2)*(n3+n4)*n5, "(n1+n2)*(n3+n4)*n5"}
+      
+
 
   };
 
